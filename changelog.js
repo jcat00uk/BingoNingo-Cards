285e35d7f264489f227fd27c9396c9b73321f53c
jcat00uk <github@thecrazycat.com>
2026-04-07 21:45:14 +0100

    Web build 20260407-2145

--- changelog.js
+++ changelog.js
@@ -1,6 +1,6 @@
 window.CHANGELOG = {
 
-  '20260407-2134': {
+  '20260407-2145': {
     changes: [
       { tag: 'update', text: 'Rebranded to BingoNingo', editions: 'all' },
       { tag: 'new', text: 'Added New Style option in Bingo Card', editions: 'all' },
